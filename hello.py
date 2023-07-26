f431c3ca4d757f0f65fa2253074311bd62703b81
Jonas <jonas.m.a.1997@gmail.com>
2023-07-26 02:28:04 -0300

    api done

--- hello.py
+++ hello.py
@@ -6,31 +6,59 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"/processImage/*": {"origins": "*"}})
+
+def orb_sim(img1, img2):
+  # SIFT is no longer available in cv2 so using ORB
+  orb = cv2.ORB_create()
+
+  # detect keypoints and descriptors
+  kp_a, desc_a = orb.detectAndCompute(img1, None)
+  kp_b, desc_b = orb.detectAndCompute(img2, None)
+
+  # define the bruteforce matcher object
+  bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    
+  #perform matches. 
+  matches = bf.match(desc_a, desc_b)
+  #Look for similar regions with distance < 50. Goes from 0 to 100 so pick a number between.
+  similar_regions = [i for i in matches if i.distance < 50]  
+  if len(matches) == 0:
+    return 0
+  return len(similar_regions) / len(matches)
+
+def structural_sim(img1, img2):
+
+  sim, diff = structural_similarity(img1, img2, full=True)
+  return sim
 
 @app.route('/processImage', methods=['POST'])
 def process_image():
     data = request.get_json()
     print(data)
-    if 'image_url' not in data:
+    if 'imageUrl' not in data:
         return jsonify({'error': 'No URL provided in the request'}), 400
     
     image_url = data['imageUrl']
-    print(image_url)
+    image_url2 = data['imageUrl2']
+
     # Download image from the provided URL using requests
-    response = requests.get(image_url)
-    if response.status_code != 200:
+    image1 = requests.get(image_url)
+    image2 = requests.get(image_url2)
+
+    if image1.status_code != 200 or image2.status_code != 200:
         return jsonify({'error': 'Failed to download the image'}), 400
     
     # Convert image data to a NumPy array
-    img_array = np.frombuffer(response.content, dtype=np.uint8)
+    img_array = np.frombuffer(image1.content, dtype=np.uint8)
     before = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
     # Assuming the 'assets/real3.jpg' file exists in the specified path
-    after = cv2.imread('assets/jaq.jpg')
+    img_array = np.frombuffer(image2.content, dtype=np.uint8)
+    after = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
-    desired_width = 800
-    desired_height = 600
+    desired_width = 1280
+    desired_height = 720
 
     resized_image_before = cv2.resize(before, (desired_width, desired_height))
     resized_image_after = cv2.resize(after, (desired_width, desired_height))
@@ -39,11 +67,18 @@ def process_image():
     before_gray = cv2.cvtColor(resized_image_before, cv2.COLOR_BGR2GRAY)
     after_gray = cv2.cvtColor(resized_image_after, cv2.COLOR_BGR2GRAY)
 
-    # Compute SSIM between two images
-    (score, diff) = structural_similarity(before_gray, after_gray, full=True)
-    print("Image similarity:", score)
 
-    return "Image similarity: " + str(score)  # Convert score to a string before returning
+
+    # Compute SSIM between two images
+    orb = orb_sim(before_gray, after_gray)
+    ssim = structural_sim(before_gray, after_gray) #1.0 means identical. Lower = not similar
+
+    print("ORB similarity:", orb)
+
+    return {
+        "ORB": str(orb),
+        "SSIM": str(ssim)
+        } # Convert score to a string before returning
 
 if __name__ == '__main__':
     app.run(debug=True)
