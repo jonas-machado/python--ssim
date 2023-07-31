dab5d0495f75d9528e4189d91d5e001205f80d6d
Jonas <jonas.m.a.1997@gmail.com>
2023-07-31 16:49:49 -0300

    ...

--- vision.py
+++ vision.py
@@ -9,23 +9,29 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={r"/processImage/*": {"origins": "*"}})
 
-os.environ['GOOGLE_APLICATION_CREDENTIALS'] = '.venv/sun-commerce-vision.json'
+os.environ["GOOGLE_APLICATION_CREDENTIALS"] = ".venv/sun-commerce-vision.json"
 client = vision.ImageAnnotatorClient()
 
-@app.route('/processImageVision', methods=['POST'])
+
+@app.route("/processImageVision", methods=["POST"])
 def process_image():
     data = request.get_json()
-    if 'imageUrl' not in data:
-        return jsonify({'error': 'No URL provided in the request'}), 400
-    
-    image_url = data['imageUrl']
+    if "imageUrl" not in data:
+        return jsonify({"error": "No URL provided in the request"}), 400
+
+    image_url = data["imageUrl"]
     print(image_url)
     image = vision.Image()
     image.source.image_uri = image_url
     response = client.label_detection(image=image)
-    return jsonify({
-        "label": response.label_annotations[1]
-    })
+    labels = response.label_annotations
+    if labels:
+        results = []
+        for label in labels:
+            print(label)
+            results.append({"label": label.description, "score": label.score})
+        return results
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
